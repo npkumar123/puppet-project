5dab0973736ce20d972c5bcc427277a5a23d5917
root <root@rhel1.(none)>
2016-06-23 21:00:18 +0530

    my initial puppet code

---
+++ test1.pp
@@ -0,0 +1,14 @@
+package{'vsftpd':
+        ensure          => "installed",
+        provider        => "yum",
+        allow_virtual   => "false",
+        }
+file{'/etc/vsftpd/vsftpd.conf':
+        ensure          => "present",
+        source          => "file:///root/puppetcode/files/vsftpd.conf",
+        require         => Package['vsftpd'],
+        }
+service{'vsftpd':
+        ensure          => "running",
+        subscribe       => File['/etc/vsftpd/vsftpd.conf'],
+        }
